2b27e30c7893b9dcb5201c24a5ebdd50d358eec0
lodobo <lodobo303@gmail.com>
2023-08-21 15:55:03 +0200

    Improved speed json-to-db
    Improved speed json-to-db

--- src/bin/json-to-db.rs
+++ src/bin/json-to-db.rs
@@ -1,5 +1,6 @@
 use indicatif::ProgressBar;
 use rdict::{
+    error::AppError,
     structs::{Word, WordInfo},
     utils,
 };
@@ -10,28 +11,9 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Open directory where databse and json file is stored
-    let rdict_dir = utils::get_home_directory()?.join(".local/share/rdict");
-    fs::create_dir_all(&rdict_dir)?;
-    // Open database
-    let mut conn = Connection::open(&rdict_dir.join("en.db"))?;
-    conn.execute(
-        "create table if not exists en (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            word TEXT NOT NULL,
-            pos TEXT NOT NULL,
-            information TEXT NOT NULL
-         )",
-        (),
-    )?;
-
-    // Open JSON File
-    let file = fs::File::open(rdict_dir.join("en.jsonl"))?;
-    let reader = BufReader::new(file);
-
-    // Begin a transaction
-    let transaction = conn.transaction()?;
+fn main() -> Result<(), AppError> {
+    let reader = read_jsonl_file("en.jsonl")?;
+    let mut conn = setup_database()?;
     let line_count = reader.lines().count() as u64;
     let progress_bar = ProgressBar::new(line_count);
     progress_bar.set_style(
@@ -41,29 +23,57 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .unwrap()
         .progress_chars("##-"),
     );
+    let reader = read_jsonl_file("en.jsonl")?;
+    process_lines(&mut conn, reader, &progress_bar)?;
 
-    let file = fs::File::open(rdict_dir.join("en.jsonl"))?;
-    let reader = BufReader::new(file);
+    Ok(())
+}
 
-    for (index, line) in reader.lines().enumerate() {
-        // deserialize, reorganize and reserialize without the useless information.
-        let word: Word = serde_json::from_str::<Word>(line.as_ref().unwrap())?;
-        let information: WordInfo = serde_json::from_str::<WordInfo>(line.as_ref().unwrap())?;
-        let information: String = serde_json::to_string(&information).unwrap();
+// Function to open the database and create the table
+fn setup_database() -> Result<Connection, AppError> {
+    let rdict_dir = utils::get_home_directory()?.join(".local/share/rdict");
+    fs::create_dir_all(&rdict_dir)?;
+    let conn = Connection::open(&rdict_dir.join("en.db"))?;
+    conn.execute(
+        "CREATE TABLE IF NOT EXISTS en (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            word TEXT NOT NULL,
+            pos TEXT NOT NULL,
+            information TEXT NOT NULL
+         )",
+        (),
+    )?;
+    Ok(conn)
+}
+
+// Function to open and read the JSONL file
+fn read_jsonl_file(filename: &str) -> Result<BufReader<fs::File>, AppError> {
+    let rdict_dir = utils::get_home_directory()?.join(".local/share/rdict");
+    let file = fs::File::open(rdict_dir.join(filename))?;
+    Ok(BufReader::new(file))
+}
+
+// Function to process each line of the JSONL file and insert into the database
+fn process_lines(conn: &mut Connection, reader: BufReader<fs::File>, progress_bar: &ProgressBar) -> Result<(), AppError> {
+    let transaction = conn.transaction()?;
+    let reader = reader.lines();
+
+    for (index, line) in reader.enumerate() {
+        let line = line?;
+        let word: Word = serde_json::from_str(&line)?;
+        let information: WordInfo = serde_json::from_str(&line)?;
+        let information: String = serde_json::to_string(&information)?;
         let information_cow: Cow<str> = Cow::Borrowed(&information);
 
-        // Queue the INSERT statement within the transaction
         transaction.execute(
             "INSERT INTO en (word, pos, information) values (?1, ?2, ?3)",
             [&word.word, &word.pos, &information_cow],
         )?;
 
-        progress_bar.set_position((index + 1) as u64);
+        progress_bar.set_position((index + 1000) as u64);
     }
 
-    // Commit the transaction
     transaction.commit()?;
     progress_bar.finish_and_clear();
-
     Ok(())
 }
