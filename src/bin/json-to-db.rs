6183a11b08c5c45c0f37b3fbc269cfa7bcd972ec
lodobo <lodobo303@gmail.com>
2023-08-21 08:08:07 +0200

    Re-organised code

--- src/bin/json-to-db.rs
+++ src/bin/json-to-db.rs
@@ -1,5 +1,8 @@
 use indicatif::ProgressBar;
-use rdict::{structs, utils};
+use rdict::{
+    structs::{Information, Word},
+    utils,
+};
 use rusqlite::{Connection, Result};
 use std::{
     fs,
@@ -7,9 +10,19 @@ use std::{
 };
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let home_dir = utils::get_home_directory()?;
-    let rdict_dir = home_dir.join(".local/share/rdict");
+    let rdict_dir = utils::get_home_directory()?.join(".local/share/rdict");
     fs::create_dir_all(&rdict_dir)?;
+    let file = fs::File::open(rdict_dir.join("en.jsonl"))?;
+    let reader = BufReader::new(&file);
+    let progress_bar = ProgressBar::new(reader.lines().count() as u64);
+    progress_bar.set_style(
+        indicatif::ProgressStyle::with_template(
+            "[{bar:40.cyan/blue}][{percent}%][{elapsed_precise}][ETA: {eta}]",
+        )
+        .unwrap()
+        .progress_chars("##-"),
+    );
+
     let mut conn = Connection::open(&rdict_dir.join("en.db"))?;
     conn.execute(
         "create table if not exists en (
@@ -21,28 +34,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         (),
     )?;
 
-    let file = fs::File::open(rdict_dir.join("en.jsonl"))?;
-    let reader = BufReader::new(file);
-
     // Begin a transaction
     let transaction = conn.transaction()?;
-    let line_count = reader.lines().count() as u64;
-    let pb = ProgressBar::new(line_count);
-    pb.set_style(
-        indicatif::ProgressStyle::with_template(
-            "[{bar:40.cyan/blue}][{percent}%][{elapsed_precise}][ETA: {eta}]",
-        )
-        .unwrap()
-        .progress_chars("##-"),
-    );
-
-    let file = fs::File::open(rdict_dir.join("en.jsonl"))?; // Open the file again
-    let reader = BufReader::new(file);
-
+    let reader = BufReader::new(&file);
     for (index, line) in reader.lines().enumerate() {
-        let word: structs::Word = serde_json::from_str::<structs::Word>(line.as_ref().unwrap())?;
-        let information: structs::Information =
-            serde_json::from_str::<structs::Information>(line.as_ref().unwrap())?;
+        let word: Word = serde_json::from_str::<Word>(line.as_ref().unwrap())?;
+        let information: Information = serde_json::from_str::<Information>(line.as_ref().unwrap())?;
         let information: String = serde_json::to_string(&information).unwrap();
 
         // Queue the INSERT statement within the transaction
@@ -50,13 +47,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             "INSERT INTO en (word, pos, information) values (?1, ?2, ?3)",
             [&word.word, &word.pos, &information],
         )?;
-
-        pb.set_position((index + 1) as u64);
+        progress_bar.set_position((index + 1) as u64);
     }
 
     // Commit the transaction
     transaction.commit()?;
-    pb.finish_and_clear();
+    progress_bar.finish_and_clear();
 
     Ok(())
 }
